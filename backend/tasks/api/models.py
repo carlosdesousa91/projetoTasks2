9cdcf324fec9d1b23714a3d00d72a58c82773565
carlosdesousa91 <31635606+carlosdesousa91@users.noreply.github.com>
2020-11-25 20:14:14 -0300

    api finalizada

--- backend/tasks/api/models.py
+++ backend/tasks/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.TextField(max_length=32, blank=False, null = False)
+    description = models.TextField(max_length=200, blank=False, null = False)
+    status = models.IntegerField(max_length=50, blank=False, null = False)
--- backend/tasks/api/views.py
+++ backend/tasks/api/views.py
@@ -2,9 +2,18 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TaskSerializer
+from .models import Task
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated,]
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAuthenticated,]
